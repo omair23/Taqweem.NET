8721c510f2ccdcf90d22daaa43f69fc632751769
Omair Kazi <omair334@gmail.com>
2017-12-05 17:20:57 +0200

    Style Fixes

--- Taqweem/Controllers/HomeController.cs
+++ Taqweem/Controllers/HomeController.cs
@@ -15,17 +15,8 @@ namespace Taqweem.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult MasjidList()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
